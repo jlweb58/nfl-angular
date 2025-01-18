96a092e39ddedfc086516199541c769214147956
John Webber <jlweb58@gmail.com>
2025-01-18 10:19:44 +0100

    Converted weekly-game-selection to interface

--- src/app/core/models/weekly-game-selection.model.ts
+++ src/app/core/models/weekly-game-selection.model.ts
@@ -2,20 +2,12 @@ import {Team} from '../../team/team.model';
 import {Game} from '../../game/game.model';
 import {GameResult} from '../../game/game-result.model';
 
-export class WeeklyGameSelection {
+export interface WeeklyGameSelection {
 
+  id: number;
   game: Game;
-
   winningTeamSelection: Team;
-
   gameResult?: GameResult;
-
   week: number;
-
-  constructor(game: Game, winningTeamSelection: Team) {
-    this.game = game;
-    this.winningTeamSelection = winningTeamSelection;
-    this.week = game.week;
-  }
 
 }
